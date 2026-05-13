5a991b7335937eeebe128b9caef14baa0378a13d
Eduardo Gouvêa <edufigue4456@gmail.com>
2026-05-13 17:30:34 -0300

    Métodos de bisseção e de Newton
    Commit dos códigos referentes a resolução da questão 2 do livro de aplicações de cálculo numérico

---
+++ metodobiseccao.py
@@ -0,0 +1,64 @@
+from time import time
+from math import log10
+
+inicio = time() # variável para calcular o tempo de execução
+
+# a função absoluto retorna o valor absoluto de algum
+# x dado como parâmetro.
+def absoluto(x):
+    if x < 0:
+        return -x
+    return x
+
+# a função f é a função modelada pelo problema 
+# e que vai ser usada para calcular sua raíz
+def f(x):
+    return (20000 * ((x * ((1 + x)**6)) / (((1 + x)**6) - 1))) - 4000
+
+
+a = 0.05
+b = 0.15
+
+fa = f(a)
+fb = f(b)
+
+parada = 0.05
+contador = 1
+itMAX = 3
+
+
+# é feita essa verificação pois se o produto entre o
+# f(a) e f(b) for maior do que 0 isso significa
+# que o TVI não está garantido e logo não pode 
+# aplicar o método
+if fa * fb > 0:
+    print('Erro! O intervalo não obedece ao TVI')
+
+else:
+
+    # um while True é feito, mas dentro dele existe a condição
+    # de parada com um break, se o valor absoluto de f(x) for menor
+    # que a parada definida como 0.05, o loop se encerra
+    while True: 
+
+        x = (a + b) / 2
+        fx = f(x)
+
+        print(f'xk = {x:.5f} | f(xk) = {fx:.5f}')
+
+        if absoluto(fx) < parada or contador == itMAX:
+            break
+
+        if fa * fx < 0:
+            b = x
+            fb = fx
+        if fb * fx < 0:
+            a = x
+            fa = fx
+    
+        contador += 1
+    
+fim = time()
+
+print(f'\nTempo de execução: {fim - inicio:.10f} segundos')
+    
---
+++ metodonewton.py
@@ -0,0 +1,40 @@
+from time import time
+from math import e
+
+inicio = time() # variável para calcular o tempo de execução
+
+# a função absoluto retorna o valor absoluto de algum
+# x dado como parâmetro.
+def absoluto(x):
+    if x < 0:
+        return -x
+    return x
+
+# função dada pela questão já substituindo os valores dados
+def f(x):
+    return (20000 * ((x * ((1 + x)**6)) / (((1 + x)**6) - 1))) - 4000
+
+# função para calcular a derivada da função
+def derivada(x):
+    h = 0.0001
+    return (f(x + h) - f(x)) / h
+
+xn = 0.1 # x0 será considerado como o ponto médio do intervalo [0.05, 0.15]
+contador = 0 # contador para limitar as iterações
+intMAX = 3 # limite de iterações dado pela questão
+parada = 0.05 # parada dada pela questão
+
+while contador < intMAX and absoluto(f(xn)) > parada:
+    
+    fx = f(xn)
+    dfx = derivada(xn)
+
+    print(f'xn = {xn} | f(xn) = {fx} | f\'(xn) = {dfx}')
+
+    xn -= fx / dfx
+
+    contador += 1
+
+fim = time()
+
+print(f'\nTempo de execução: {fim - inicio:.10f} segundos')
